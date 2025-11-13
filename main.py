1a49a5a411112cddf1166c94b79665851e34aaf6
Naftali Horviz <n0548498093@gmail.com>
2025-11-13 12:43:34 +0200

    all project

---
+++ main.py
@@ -0,0 +1,57 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from utils.encrypt import caesar_encrypt, fence_encrypt, fence_decrypt
+app= FastAPI()
+path_names = "names.txt"
+
+# mode = "encrypt" | "decrypt"
+
+class DataCaesar(BaseModel):
+    text: str
+    offset: int
+    mode: str
+
+class DataFence(BaseModel):
+    text: str
+    
+@app.get("/test")
+def test():
+    return { "msg": "hi from test"}
+    
+@app.get("/test/{name}")
+def test_username(name: str):
+    with open(path_names, 'a') as f:
+        f.write(name + "\n")
+    return { "msg":"saved user",}
+
+@app.post("/caesar")
+def caesar_cipher(data: DataCaesar):
+    result =caesar_encrypt(data.text, data.offset, data.mode)
+    return {f"{data.mode}ed_text:": result}
+
+@app.get("/fence/encrypt")
+def caesar_cipher(text: str):
+    result = fence_encrypt(text)
+    return {"encrypted_text": result}
+
+@app.post("/fence/decrypt")
+def caesar_cipher(text: DataFence):
+    result = fence_decrypt(text.text)
+    return {"decrypted_text": result}
+    
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        "main:app",
+        host="localhost",
+        port=8012,
+        reload=True
+    )
---
+++ utils/encrypt.py
@@ -0,0 +1,43 @@
+
+
+def caesar_encrypt(text: str, offset: str, mode: str):
+    result = ""
+    for i in text.upper():
+        if i == ' ':
+            result += i
+            continue
+        char = chr(ord(i) - offset) if mode == "decrypt" else chr(ord(i) + offset)
+        if ord(char) < ord('A'):
+            num_char = abs(ord(char) - ord('A'))
+            char = chr(ord('Z') + 1 - num_char)
+        elif ord(char) > ord('Z'):
+            num_char = ord(char) - ord('Z')
+            char = chr(ord('A') - 1 + num_char)
+        result += char
+    return result
+
+def fence_encrypt(text: str):
+    text = text.replace(' ', '')
+    flag = True
+    even, no_even ="",""
+    for i in text:
+        if flag:
+            even += i
+        else:
+            no_even += i
+        flag = not flag
+    return even + no_even
+      
+def fence_decrypt(text:str):
+    result = text
+    len_text = len(text) // 2 + 1
+    if len(text) % 2 != 0:
+        len_text += 1
+    for _ in range(len_text):
+        result = fence_encrypt(result)
+    return result
+
+# print(fence_encrypt("ab ac a"))
+print(fence_encrypt("hi there1"))
+print(fence_decrypt("hteeihr1"))
+# print(caesar_encrypt('bc', 2, "decrypt"))
